0aae4da1743863899b7dccc89ff952312104b6d9
Ondřej Čertík <ondrej@certik.us>
2023-09-28 18:31:09 -0600

    X

--- driver.c
+++ driver.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 extern int32_t run_wasm_native_binary(size_t len, uint8_t *guest);
@@ -5,6 +6,6 @@ extern int32_t run_wasm_native_binary(size_t len, uint8_t *guest);
 int main() {
   extern const char guest[];
   extern const size_t guest_len;
-  int32_t result = run_wasm_native_binary(guest_len, guest)
+  int32_t result = run_wasm_native_binary(guest_len, (uint8_t*)guest);
   return result;
 }
