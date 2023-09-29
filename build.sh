72a772d68b5a5ea95fa509699167867ff410f8d6
Ondřej Čertík <ondrej@certik.us>
2023-09-28 18:36:25 -0600

    release

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ wasmtime compile guest.wasm -o guest.cwasm
 cd ..
 
 cd mylib
-cargo build --offline
+cargo build --release --offline
 cd ..
 
-strip mylib/target/debug/libmylib.a
+strip mylib/target/release/libmylib.a
 
-cargo run --offline
+cargo run --release --offline
 
 bin2c guest < src/guest.cwasm > guest.c
 clang -c guest.c -o guest.o
 clang -c driver.c -o driver.o
-clang -o driver driver.o guest.o mylib/target/debug/libmylib.a
+clang -o driver driver.o guest.o mylib/target/release/libmylib.a
 ls -h driver
 strip driver
 ls -h driver
